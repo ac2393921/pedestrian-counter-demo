50f9708bdc066217a203ed9c2fc216852a270c3d
ac2393921 <ac2393921@gmail.com>
2021-11-21 20:05:14 +0900

    update: 各モジュールを使用し歩行者の人数を描画するmain関数の実装

--- src/main.py
+++ src/main.py
@@ -1,52 +1,33 @@
-import sys
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import cv2
-import pandas as pd
+import time
+
+
+from utils import PedestrianCounter, read_tracking, read_baseline
+
+
+ALPHA = 0.2
+DATA_PATH = '../data/'
 
 
 def main():
-    input_vide = cv2.VideoCapture('../data/video/demo.mp4')
+    start_time = time.time()
 
-    if not input_vide.isOpened():
-        sys.exit()
+    tracking_list = read_tracking(f'{DATA_PATH}tracking/demo.txt')
+    baseline = read_baseline(f'{DATA_PATH}csv/baseline.csv')
 
-    frame_size = int(input_vide.get(cv2.CAP_PROP_FRAME_WIDTH)), int(input_vide.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frame_rate = int(input_vide.get(cv2.CAP_PROP_FPS))
-    frame_count = int(input_vide.get(cv2.CAP_PROP_FRAME_COUNT))
+    pc = PedestrianCounter(
+        tracking_list,
+        baseline,
+        f'{DATA_PATH}video/demo.mp4',
+        f'{DATA_PATH}output/output.mp4')
 
-    with open('../data/tracking/demo.txt', "r") as f:
-        lines = f.read().split('\n')
-        tracking_list = [list(map(int, line.split(' ')))[:6] for line in lines]
+    pc.draw()
+    pc.close_video()
 
-    df = pd.DataFrame(tracking_list, columns=['frame_id', 'tracking_id', 'x', 'y', 'w', 'h'])
-    df['center_x'] = df['x'] + df['w'] / 2
-
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    output_video = cv2.VideoWriter('../data/output/output.mp4', fourcc, frame_rate, frame_size)
-
-    frame_id = 1
-
-    while True:
-        ret, frame = input_vide.read()
-
-        if ret:
-            current_frame = df.loc[df['frame_id'] == frame_id, ['frame_id', 'tracking_id', 'x', 'y', 'w', 'h', 'center_x']]
-
-            for c in current_frame.itertuples():
-                cv2.rectangle(frame, (c.x, c.y), (c.x+c.w, c.y+c.h), (0, 255, 0), 2)
-                cv2.circle(frame, center=(int(c.center_x), int(c.y+c.h)), radius=10,
-                            color=(0, 0, 255), thickness=-1, lineType=cv2.cv2.LINE_4)
-
-            output_video.write(frame)
-
-            print(f"{str(frame_id)}/{str(frame_count)}")
-
-            frame_id += 1
-        else:
-            break   
-
-    input_vide.release()
-    output_video.release()
+    end_time = time.time()
+    print("time is ", end_time - start_time, "'s")
 
 
 if __name__ == "__main__":
